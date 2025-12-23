3be53b7cadde74b9e1a6c2df8d7bd58b6058b86d
MageshwarSV <mageshwarsv@example.com>
2025-12-23 16:16:16 +0530

    Added complete project - all Python backend, React UI, configs, routes, and migrations

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 30012,   // 👈 change default port
+    host: true,    // 👈 allows external access (e.g. http://103.14.123.44:30012)
+    allowedHosts: [
+      'hoselike-tonetically-kylah.ngrok-free.dev', // ngrok domain
+      'boostentry-ui.loca.lt', // localtunnel domain
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://103.14.123.44:30010',
+        changeOrigin: true,
+      },
+    },
+  },
+})
